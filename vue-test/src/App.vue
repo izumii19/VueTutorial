ddd166836e911bb002c9fbaae64404619f6a1ea5
izumiito19 <izumiito19@gmail.com>
2020-02-13 17:14:43 +0900

    sample1:データをバインドさせた値を表示

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>|
+      <router-link to="/sample1">sample1</router-link>|
+      <router-link to="/sample2">sample2</router-link>
     </div>
     <router-view/>
   </div>
---
+++ vue-test/src/views/sample1.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+    <p>{{ message2 }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Hello Vue.js!",
+      message2: "データをバインドさせて表示してます"
+    };
+  }
+};
+</script>
+ 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  border: 1px solid blue;
+}
+p {
+  color: blue;
+}
+</style>
